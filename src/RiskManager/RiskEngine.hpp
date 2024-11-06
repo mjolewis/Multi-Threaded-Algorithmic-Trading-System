f713157a9fe3e55f87ff058a36324f8425a067ac
mlewis <mjolewis@gmail.com>
2024-11-06 17:55:10 -0500

    123-risk-management-create-initial-structure-for-component

---
+++ src/RiskManager/RiskEngine.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Michael Lewis on 11/6/24.
+//
+
+#ifndef BEACONTECH_RISKENGINE_HPP
+#define BEACONTECH_RISKENGINE_HPP
+
+
+class RiskEngine
+{
+
+};
+
+
+#endif //BEACONTECH_RISKENGINE_HPP
---
+++ src/RiskManager/RiskMain.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Michael Lewis on 11/6/24.
+//
+
+#include "iostream"
+
+int main()
+{
+    std::cout << "TEST" << std::endl;
+    return 0;
+}
